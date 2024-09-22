4a62e9f9a655dee8eee50fbc355722338f701795
Fikri Muhammad Fadhilah <179136498+fikrimf280305@users.noreply.github.com>
2024-09-22 19:49:18 +0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		firstName string = "Suzumi"
+
+		lastName string = "Morizuki"
+
+		birthday string = "August 31"
+	)
+
+	fmt.Printf("Her name is %s %s and her birthday is %s.\n", firstName, lastName, birthday)
+}
